cb2203143f66cc5cfea874793fdc497416983398
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2026-01-14 16:01:04 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import {Routes, Route} from 'react-router-dom'
+import Login from './pages/login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      This is gonna be the place from where the hooter will rise and shine
-    </>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+    </Routes>
   )
 }
 
---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+import {React} from "react";
+
+const Login = ()=>{
+    return(<>
+    Hi this is the login page
+    </>)
+}
+
+export default Login;
