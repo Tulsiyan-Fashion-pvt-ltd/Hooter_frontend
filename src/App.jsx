71d5488eb2f225e0cf2a54cce0cf5571b29bbee6
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2026-01-16 18:19:42 +0530

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"; 
 import Login from "./pages/login";
 import Homepage from "./homepage";
+import Register from "./pages/register";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes> 
       <Route path="/" element={<Homepage/>} /> 
       <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register/>}/>
     </Routes>
   );
 } 
---
+++ src/pages/register.jsx
@@ -0,0 +1,71 @@
+import "../css/pages/register.css";
+import {Link} from "react-router-dom";
+
+const Register = () => {
+    return (
+        <div id="register-container">
+            <div className="container">
+                <form>
+                    <div className="section">
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="text" placeholder="Full name" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" placeholder="User Designation" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="text" placeholder="Contact Number" />
+                            </div>
+                            <div className="form-group">
+                                <input type="email" placeholder="Email Address" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group half-width">
+                                <input type="text" placeholder="If POC For Future" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="section">
+                        <h2 className="section-title">Business Details</h2>
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="text" placeholder="Business Name" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" placeholder="Business Start Date" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="text" placeholder="GST Number" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" placeholder="Number Of Active Employee" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="text" placeholder="Business Niche" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" placeholder="Business Registered Address" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="footer">
+                        <Link to="#" className="back-link">Back</Link>
+                        <button type="submit" className="next-btn"></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
