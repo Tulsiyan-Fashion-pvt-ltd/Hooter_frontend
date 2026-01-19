330798486a76254cbe241c99f7569f0c0a2c0f84
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2026-01-19 10:47:06 +0530

    signup requests page and setup

--- src/components/proceed-bttn.jsx
+++ src/components/proceed-bttn.jsx
@@ -1,9 +1,9 @@
 import styles from '../css/layout/ArrowProceedBttn.module.css';
 import clsx from 'clsx'
 
-export function ArrowProceedBttn({className}){
+export function ArrowProceedBttn({className, onClick}){
 
     return(
-        <button type="submit" className={clsx(styles.arrowBttn, className)}></button>
+        <button type="submit" className={clsx(styles.arrowBttn, className)} onClick={onClick}></button>
     )
 }
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
 import { ArrowProceedBttn } from '../components/proceed-bttn';
 import styles from '../css/pages/Signup.module.css'
 
 const Signup = () => {
+    const submit = async ()=>{
+        console.log('clicking')
+        const name = document.querySelector('#name');
+        const response = await fetch('http://localhost:8800/signup', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            credentials: "include", 
+            body: JSON.stringify({'name': name.value})
+        })
+    }
+
     return(
         <div className={styles.superContainer}>
         <div id="signup-container" className={styles.container}>
@@ -10,13 +22,15 @@ const Signup = () => {
 
             <div id="identification-credentials" className={styles.identificationContainer}>
                 <div id="input-fields" className={styles.inputFields}>
-                    <input type="text" id="mail" name='mail' placeholder='Enter your e-mail' />
-                    <input type="text" id="num" name='num' placeholder='Enter your phone number' />
-                    <input type="text" id="password" name='password' placeholder='Create password'/>
-                    <input type="text" id="conf-pass" name='conf-password' placeholder='Confirm password'/>
+                    <input type="text" id="name" name='name' placeholder='Name'/>
+                    <input type="text" id="mail" name='mail' placeholder='E-mail' required />
+                    <input type="text" id="num" name='num' placeholder='Phone number' required />
+                    <input type="text" id="designation" name='designation' placeholder='Designation (optional)' />
+                    <input type="text" id="password" name='password' placeholder='Create password' required />
+                    <input type="text" id="conf-pass" name='conf-password' placeholder='Confirm password' required />
                 </div>
                 <div className={styles.buttonContainer}>
-                <ArrowProceedBttn className={styles.proceedButton}/>
+                <ArrowProceedBttn className={styles.proceedButton} onClick={submit}/>
                 </div>
             </div>
         </div>
