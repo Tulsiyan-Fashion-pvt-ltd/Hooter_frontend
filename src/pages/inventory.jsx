eba5fac1cb0ed7d29d7b20abfdd8cc7bf08914ff
Darshan <darshanr181204@gmail.com>
2026-04-05 00:56:23 +0530

    Fix : Inventory UI corrections
    Increased the gap b/w the sidebar and the contents of the gap (globally)
    Redesigning the inventory page wrt to the design provided.
    Major changes :
    1. Added svgs to all the containers and replaced empty placeholders (as of now till the overview section)
    2.  Made changes to the classes for each section and cards for inventory
    Right now the design has been fixed till the overview section of the inventory page

--- src/pages/inventory.jsx
+++ src/pages/inventory.jsx
@@ -5,6 +5,7 @@ export default function Inventory() {
         <div className={styles.InventoryGlobalContainer}>
             <div className={styles.pageWrapper}>
                 <div className={styles.customBox}>
+                    <img src="/src/assets/icons/hero.svg" className={styles.heroBg} alt="" />
                     <div className={styles.leftCustom}>
                         <h1>Manage Inventory</h1>
                         <p>
@@ -22,8 +23,8 @@ export default function Inventory() {
                 <div className={styles.overview}>
                     <div className={styles.firstRow}>
                         <div className={styles.overLeft}>
-                            <div className={styles.overIcon}></div>
-                            <p>
+                            <img src="/src/assets/icons/inventory.svg" className={styles.iconSm} alt="" />
+                            <p className={styles.textSm}>
                                 Have unique products to sell? Choose from the options below
                             </p>
                         </div>
@@ -31,15 +32,15 @@ export default function Inventory() {
                     </div>
 
                     <div className={styles.secondRow}>
-                        <h2 className={styles.overH}>Overview</h2>
+                        <h2 className={styles.textOverview}>Overview</h2>
 
                         <div className={styles.ovrBx}>
                             {/* Box 1 */}
                             <div className={styles.overBox}>
                                 <div className={styles.inBox1}>
-                                    <div className={styles.inSb}></div>
-                                    <h3>Sellable Inventory</h3>
-                                    <div className={styles.menu}>⋮</div>
+                                     <img src="/src/assets/icons/note.svg" className={styles.iconSm} alt="" />
+                                    <h3 className={styles.textCard}>Sellable Inventory</h3>
+                                    <button className={styles.menuBtn}>⋮</button>
                                 </div>
 
                                 <div className={styles.line}></div>
@@ -47,7 +48,8 @@ export default function Inventory() {
                                 <div className={styles.inBox2}>
                                     <h3>38</h3>
                                     <button>
-                                        View details <span>→</span>
+                                        View details 
+                                        <img src="/src/assets/icons/arrow_black.svg" className={styles.arrowIcon} alt="" />
                                     </button>
                                 </div>
                             </div>
@@ -55,9 +57,9 @@ export default function Inventory() {
                             {/* Box 2 */}
                             <div className={styles.overBox}>
                                 <div className={styles.inBox1}>
-                                    <div className={styles.inSb}></div>
-                                    <h3>Out of Stock</h3>
-                                    <div className={styles.menu}>⋮</div>
+                                    <img src="/src/assets/icons/note.svg" className={styles.iconSm} alt="" />
+                                    <h3 className={styles.textCard}>Out of Stock</h3>
+                                    <button className={styles.menuBtn}>⋮</button>
                                 </div>
 
                                 <div className={styles.line}></div>
@@ -65,7 +67,8 @@ export default function Inventory() {
                                 <div className={styles.inBox2}>
                                     <h3>6 SKU’s</h3>
                                     <button>
-                                        View details <span>→</span>
+                                        View details 
+                                        <img src="/src/assets/icons/arrow_black.svg" className={styles.arrowIcon} alt="" />
                                     </button>
                                 </div>
                             </div>
@@ -73,9 +76,9 @@ export default function Inventory() {
                             {/* Box 3 */}
                             <div className={styles.overBox}>
                                 <div className={styles.inBox1}>
-                                    <div className={styles.inSb}></div>
-                                    <h3>Low Stocks</h3>
-                                    <div className={styles.menu}>⋮</div>
+                                    <img src="/src/assets/icons/note.svg" className={styles.iconSm} alt="" />
+                                    <h3 className={styles.textCard}>Low Stocks</h3>
+                                    <button className={styles.menuBtn}>⋮</button>
                                 </div>
 
                                 <div className={styles.line}></div>
@@ -83,7 +86,8 @@ export default function Inventory() {
                                 <div className={styles.inBox2}>
                                     <h3>26 SKU’s</h3>
                                     <button>
-                                        View details <span>→</span>
+                                        View details
+                                        <img src="/src/assets/icons/arrow_black.svg" className={styles.arrowIcon} alt="" />
                                     </button>
                                 </div>
                             </div>
